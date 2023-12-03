6fc585f5f5bd66eced716da5085a6f8852430be9
Saurabh Sharma <39699314+saurabhsharma2395@users.noreply.github.com>
2023-12-03 11:43:04 -0500

    Updates

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,19 +1,71 @@
 /******************************************************************************
  *
- *	ITE5315 – Assignment 4
- *	I declare that this assignment is my own work in accordance with Humber Academic Policy.
- *	No part of this assignment has been copied manually or electronically from any other source
- *	(including web sites) or distributed to other students.
+ *  ITE5315 – Project 
+ *  I declare that this assignment is my own work in accordance with Humber Academic Policy. 
+ *  No part of this assignment has been copied manually or electronically from any other source 
+ *  (including web sites) or distributed to other students.
  *
- *	Name: Saurabh Sharma 	Student ID: N01543808	Date: November 27, 2023
- *
+ *	Name: Saurabh Sharma 	Student ID: N01543808	Date: December 03, 2023
  *
  ******************************************************************************/
-let mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-let restaurantSchema = mongoose.Schema({
-    
+const gradeSchema = new mongoose.Schema({
+  date: {
+    type: Date,
+    required: true
+  },
+  grade: {
+    type: String,
+    required: true
+  },
+  score: {
+    type: Number,
+    required: true
+  }
 });
 
+const restaurantSchema = new mongoose.Schema({
+  address: {
+    building: {
+      type: String,
+      required: true
+    },
+    coord: {
+      type: [Number],
+      required: true
+    },
+    street: {
+      type: String,
+      required: true
+    },
+    zipcode: {
+      type: String,
+      required: true
+    },
+    borough: {
+      type: String,
+      required: true
+    }
+  },
+  cuisine: {
+    type: String,
+    required: true
+  },
+  grades: {
+    type: [gradeSchema],
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  restaurant_id: {
+    type: String,
+    required: true
+  }
+});
 
-let books = module.exports = mongoose.model("restaurant", restaurantSchema);
+const restaurant = mongoose.model('Restaurant', restaurantSchema);
+
+module.exports = restaurant;
